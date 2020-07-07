7347d0e51980e23252e480d36f6c096615afe8ea
Skhmt <michael.shinn@outlook.com>
2020-07-07 11:01:26 -0700

    modified sw ver

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * reading: https://css-tricks.com/serviceworker-for-offline/
  */
 
-const version = 'md-1.0.0-emde'
+const version = 'md-1.0.0'
 const cacheURLs = [
     '/pad',
     '/pad/md.js',
